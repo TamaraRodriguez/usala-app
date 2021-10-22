13895b29bb283fdaccaebdfae77d9dd37c207dc3
Julian Preiss <julianpreiss23@gmail.com>
2021-10-22 11:34:56 -0300

    cambios en RegistroUser

--- src/views/auth/RegistroUser.vue
+++ src/views/auth/RegistroUser.vue
@@ -7,14 +7,21 @@
                     <div class="row mb-3">
                         <div class="col-md-6 text-start mb-3">
                             <label for="name" class="form-label">Nombre</label>
-                            <select id="name" name="name" class="form-select"  v-model="name">
-                                <option selected value="sala">Sala de ensayo</option>
-                                <option value="estudio">Estudio de grabación</option>
-                            </select>
+                            <input type="text" id="name" name="name" class="form-control" v-model="name">
                         </div>
                         <div class="mb-3 col-md-6 text-start">
                             <label for="lastname" class="form-label">Apellido</label>
                             <input type="text" id="lastname" name="lastname" class="form-control" v-model="lastname">
+                        </div>
+
+                        <div class="mb-3 col-md-6 text-start">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="email" id="email" name="email" class="form-control" v-model="email">
+                        </div>
+
+                        <div class=" text-start">
+                            <label for="pass" class="form-label">Password</label>
+                            <input type="password" id="pass" name="pass" class="form-control" v-model="pass">
                         </div>
                     
                         <div class="mb-3 col-md-6 text-start">
@@ -22,7 +29,7 @@
                             <input type="text" id="age" name="age" class="form-control" v-model="age">
                         </div>
 
-                        <div class=" text-start">
+                        <div class="text-start">
                             <label for="img" class="form-label">Foto</label>
                             <input type="file" id="img" name="img" class="form-control" multiple>
                         </div>
@@ -30,26 +37,10 @@
                     </div>
                         <div class="row">
                             <div class="col-md-9 mb-3 text-start">
-                                <label for="direccion" class="form-label">Dirección</label>
-                                <input name="direccion" id="direccion" class="form-control" v-model="direccion">
-                            </div>
-                            <div class="col-md-3 text-start">
-                                <label for="precio" class="form-label">Precio por hora</label>
-                                <input type="precio" class="form-control" id="precio"  v-model="precio">
+                                <label for="address" class="form-label">Dirección</label>
+                                <input name="address" id="address" class="form-control" v-model="address">
                             </div>
                         </div>
-                    <div class="row">
-                        <div class="my-3 text-start">
-                        <label for="" class="mb-1">Otros datos</label><br>
-                        <label>
-                            <input type="checkbox" id="wifi" value="wifi"  v-model="wifi"> Wifi
-                        </label><br>
-                        <label>
-                            <input type="checkbox" id="alquilerinstrumentos" value="alquilerinstrumentos"  v-model="alquilerinstrumentos"> Alquiler de instrumentos
-                        </label>
-                    </div>
-                    <button class="btn btn-usala btn-lg mt-3" type="submit">Registrar Sala</button>
-                    </div>
                     
                 </form>
                 <div class="py-3"></div>
@@ -68,9 +59,9 @@ export default {
             lastname: '',
             age: '',
             img: '',
-            direccion: '',
-            precio: '',
-            wifi: '',
+            address: '',
+            email: '',
+            pass: '',
             alquilerinstrumentos: ''
         }
     },
