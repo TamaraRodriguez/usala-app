5520edbe974c4d8bb58d0cf928b50de21b957e59
Nahuel Chierichetti <nahuelchierichetti@gmail.com>
2021-10-22 10:49:11 -0300

    Agregado de favoritos

---
+++ src/views/Favoritos.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>Favoritos</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam saepe, excepturi rerum est distinctio qui totam odio ad maxime repellat, porro veritatis iure quas quae dignissimos sit incidunt molestias odit!
+    Sed facilt porro, provident minus ullam quis? Molestias facilis exercitationem error voluptatibus excepturi asperiores, optio repudiandae, et cum totam esse eligendi enim reiciendis dolor ab molestiae qui.
+    Sequi vel accusantium, ipsum saepe explicabo placeat minima. Eaque alias error, optio voluptates dolor, quia voluptatem ipsum similique modi aperiam a? Quod similique fuga molestiae accusantium corrupti cupiditate, saepe exercitationem!
+    Et qui vitae, expedita doloremque hic dolor, architecto recusandae molestiae non nostrum ullam necessitatibus, nemo ipsa similique maiores quisquam atque officiis porro ratione ex. Corrupti eveniet accusantium fuga eius quaerat.
+    Consectetur sit sapiente laudantium numquam laborum. Nemo maxime rerum aut eos recusandae, impedit aliquid! Doloribus ipsa veniam odio perferendis, corporis quas velit molestiae aut dolorem repudiandae impedit voluptate cumque minima.
+    Deleniti magni dolorem voluptatem, quaerat quam iste magnam, iure facere laborum ipsam quos debitis aut tempore omnis voluptate minima, eos pariatur aspernatur veritatis consequuntur. Nemo id eaque aut velit accusantium.
+    Ipsam illo, laudfdassssssfga rgcvz antium quasi pariatur necessitatibus illum provident ducimus, consequuntur delectus aliquid ab, eaque at quisquam consequatur id veniam cum in voluptatem temporibus voluptates aut earum magni? Totam, alias architecto?
+    Modi ipsa, asperiores laudantium odio hic nostrum non excepturi animi! Repudiandae nisi consequatur voluptate, id laudantium cum. Voluptatibus eligendi suscipit exercitationem ab magnam eaque nam facilis! Sed officia repellendus placeat.
+    Natus amet blanditiis officia sint dolorem veritatis non tempore atque suscipit? Repellendus distinctio provident inventore ducimus molestiae quas sequi pariatur neque labore! Laudantium laboriosam repellendus aut doloribus illum earum aliquam?
+    Ad possimus officia tempora illum consequuntur cupiditate ipsa omnis nemo dolore, nostrum sequi nesciunt molestias vel autem! Eos, sunt eum? Inventore deserunt praesentium perspiciatis perferendis unde aperiam, laudantium voluptas sunt.
+    Eius quasi cum, perferendis dolores quidem esse commodi perspiciatis labore hic minus iste accusantium eos natus in ducimus porro reiciendis! Qui praesentium maxime aperiam, nihil accusantium voluptates iusto ad dolore.</p>
+    </div>
+</template>
